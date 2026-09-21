1c000646d51e155b166b35039333e9bc07143008
Tolotra Rakotomalala Andriamahefa <tolotra025@gmail.com>
2026-09-21 13:40:05 +0300

    chore(backend + database): creating prismaService to interact with the database

--- one/srcs/backend/src/app.module.ts
+++ one/srcs/backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './prisma/prisma.module.js';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 
