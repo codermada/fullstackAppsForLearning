2b99ec7185f5da8bf6d6d730440d2c2357ceb7d4
Tolotra Rakotomalala Andriamahefa <tolotra025@gmail.com>
2026-09-21 13:16:32 +0300

    chore(backend + database): setting up prisma + postgres

---
+++ one/srcs/backend/prisma/migrations/20260921100120_add_table_task/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "task" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "completedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
